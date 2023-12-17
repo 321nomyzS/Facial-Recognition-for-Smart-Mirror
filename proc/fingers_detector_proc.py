d95e8810f5dde7344ae3c1695eeb48d111b5e558
321nomyzS <balinskiszymon@gmail.com>
2023-12-17 19:11:08 +0100

    Create fingers detector function

--- proc/fingers_detector_proc.py
+++ proc/fingers_detector_proc.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
-
+from collections import Counter
 
 class HandDetector:
     def __init__(self):
@@ -39,27 +39,39 @@ def fingers_detector_function(shared_value):
     detector = HandDetector()
 
     while True:
-        success, img = cam.read()
+        data = []
+        start_time = time.time()
+        sec = 3
 
-        img = detector.find_hands(img, draw=False)
-        finger_list = detector.find_position(img, draw=False)
+        while True:
+            success, img = cam.read()
+            img = detector.find_hands(img, draw=False)
+            finger_list = detector.find_position(img, draw=False)
 
-        if len(finger_list) > 0:
-            # https://developers.google.com/static/mediapipe/images/solutions/hand-landmarks.png
+            if len(finger_list) > 0:
+                # https://developers.google.com/static/mediapipe/images/solutions/hand-landmarks.png
 
-            # Handle thumb
-            if finger_list[4][1] > finger_list[17][1]:
-                fingers_status = [finger_list[3][1] < finger_list[4][1]]
-            else:
-                fingers_status = [finger_list[3][1] > finger_list[4][1]]
+                # Handle thumb
+                if finger_list[4][1] > finger_list[17][1]:
+                    fingers_status = [finger_list[3][1] < finger_list[4][1]]
+                else:
+                    fingers_status = [finger_list[3][1] > finger_list[4][1]]
 
-            # Handle rest fingers
-            fingers_status += [finger_list[8][2] < finger_list[6][2], finger_list[12][2] < finger_list[10][2],
-                               finger_list[16][2] < finger_list[14][2], finger_list[20][2] < finger_list[18][2]]
+                # Handle rest fingers
+                fingers_status += [finger_list[8][2] < finger_list[6][2], finger_list[12][2] < finger_list[10][2],
+                                   finger_list[16][2] < finger_list[14][2], finger_list[20][2] < finger_list[18][2]]
 
-            fingers_status = list(map(int, fingers_status))
-            shared_value.value = sum(fingers_status)
+                fingers_status = list(map(int, fingers_status))
+                data.append(sum(fingers_status))
 
+            current_time = time.time()
+            delta = current_time - start_time
+            if delta >= sec:
+                break
 
-
-
+        if len(data) == 0:
+            shared_value.value = 0
+        else:
+            count = Counter(data)
+            most_common = count.most_common(1)[0][0]
+            shared_value.value = most_common
